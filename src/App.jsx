c59053b6f83ac7d7eb1a9b89b205080fc6f92521
coffmanjrp <coffmanjrp@gmail.com>
2021-05-10 22:34:37 -0700

    Create custom themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { Create, Notes } from './pages';
+import { purple } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fefefe',
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route exact path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route exact path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Button, Container, Typography } from '@material-ui/core';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({}));
 
 export default function Create() {
-  return <div>Create Page</div>;
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Typography
+        className={''}
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        type="submit"
+        className={''}
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+        onClick={() => console.log('clicked')}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
