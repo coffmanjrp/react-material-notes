cf0dfa9341739ae596dcc662662a56eb956d567b
coffmanjrp <coffmanjrp@gmail.com>
2021-05-11 19:07:10 -0700

    Set json-server and show data on notes page

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import {
   Button,
   Container,
@@ -23,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
   const [titleError, setTitleError] = useState(false);
@@ -43,7 +45,15 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(title, details, category);
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title,
+          details,
+          category,
+        }),
+      }).then(() => history.push('/'));
     }
   };
 
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Card } from '@material-ui/core';
 
 export default function Notes() {
-  return <div>Notes Page</div>;
+  const [notes, setNotes] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/notes')
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <Container>
+      <Grid container>
+        {notes.length > 0 &&
+          notes.map((note) => (
+            <Grid key={note.id} item xs={12} sm={6} md={3} lg={4}>
+              <Card>{note.title}</Card>
+            </Grid>
+          ))}
+      </Grid>
+    </Container>
+  );
 }
