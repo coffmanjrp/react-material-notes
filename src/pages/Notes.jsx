0a567cfe169d7e389c6d25904cdd810b5a156601
coffmanjrp <coffmanjrp@gmail.com>
2021-05-11 19:25:06 -0700

    Create NoteCard component and implement delete button

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grid, Card } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
+import { NoteCard } from '../components';
 
 export default function Notes() {
-  const [notes, setNotes] = useState('');
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8000/notes')
@@ -12,13 +13,22 @@ export default function Notes() {
     // eslint-disable-next-line
   }, []);
 
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:8000/notes/${id}`, {
+      method: 'DELETE',
+    });
+
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.length > 0 &&
           notes.map((note) => (
             <Grid key={note.id} item xs={12} sm={6} md={3} lg={4}>
-              <Card>{note.title}</Card>
+              <NoteCard note={note} handleDelete={handleDelete} />
             </Grid>
           ))}
       </Grid>
